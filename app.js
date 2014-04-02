3a8d2acce34679645aa85a3aac7aaf7a0bd83299
Adam Magaluk <AdamMagaluK@gmail.com>
2014-04-02 09:38:04 -0400

    Added back the logic for the lcd update

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 var HelloApp = module.exports = function() {
   this.name = 'hello';
 };
@@ -7,6 +6,22 @@ HelloApp.prototype.init = function(elroy) {
 
   elroy.observe('type="lcddisplay"').subscribe(function(lcd){
     elroy.expose(lcd);
+    
+    var count = 0;
+    var names = ['updateValA','updateValB'];
+    elroy
+      .observe('type="photosensor"')
+      .take(2)
+      .subscribe(function(photosensor){
+	elroy.expose(photosensor);
+	
+	var func = names[count];
+	photosensor.on('update',function(val){
+	  lcd.call(func,val,function(){});
+	});
+
+	count++;
+      });
   });
 
 };
